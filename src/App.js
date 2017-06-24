2f53c3e2e4ba4aaa8b9675b068aa1c8fb147a7d4
Crystal O'Mara <omaracrystal@gmail.com>
2017-06-23 23:46:59 -0600

    set up routes to home and about and rendered app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import HomePage from './pages/home-page';
+import AboutPage from './pages/about-page';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+
+        <div>
+            <header>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+            </header>
+
+            <main>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/about" component={AboutPage} />
+            </main>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
     );
   }
 }
